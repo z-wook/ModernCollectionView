c1505a3208d2df9732d6abffaebb31971d80b3a9
jiwook_han <jiwook.han.dev@gmail.com>
2023-08-21 02:59:48 +0900

    init

---
+++ ModernCollectionView/ViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ViewController.swift
+//  ModernCollectionView
+//
+//  Copyright (c) 2023 z-wook. All right reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+}
